2b7d3ccc4412e66401aaae8b02ff7f4184d17a8b
Dusan <dusanjankovic877@gmail.com>
2021-03-29 18:16:55 +0200

    displaying error messages for creating a post form

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -2,27 +2,30 @@
   <b-form @submit.prevent class="m-auto">
     <h1>Kreiraj objavu</h1>
     <div class="post-inputs row">
-      <div class="title-input col-lg-5 ml-auto">
+      <div class="title-input col-lg-3 m-auto">
         <b-form-group
           id="title-group"
           label="Unesite Naslov"
           label-for="title"
         >
           <b-form-input id="title" v-model="title" label="title"></b-form-input>
+        <b-alert variant="danger" :show="!!errorTitle">{{errorTitle}}</b-alert>
         </b-form-group>
         <!-- <Error>title must be more than 10 characters</Error> -->
       </div>
-      <div class="description-input col-lg-5 ml-auto">
+      <div class="description-input col-lg-3 m-auto">
         <b-form-group
           id="description-group"
           label="Unesite Opis"
           label-for="description"
         >
           <b-form-input id="description" v-model="description" label="description"></b-form-input>
+        <b-alert variant="danger" :show="!!errorDescription">{{errorDescription}}</b-alert>
+
         </b-form-group>
         <!-- <Error>title must be more than 10 characters</Error> -->
       </div>
-      <div class="img-input col-lg-3 mr-auto">
+      <div class="img-input col-lg-3 m-auto">
         <b-form-group
           id="file-group"
           label="Unesite putanju slike"
@@ -36,18 +39,25 @@
             placeholder="Choose a file or drop it here..."
             drop-placeholder="Drop file here..."
           ></b-form-file> -->
+        <b-alert variant="danger" :show="!!errorImageUrl">{{errorImageUrl}}</b-alert>
+
         </b-form-group>
         <!-- <Error>post must have image</Error> -->
       </div>
     </div>
     <quill-editor
         name="aquill1"
-        class="col-lg-12 mb-5"
+        class="col-lg-12"
         ref="myQuillEditor"
         v-model="content"
         :options="editorOption"
     />
-    <div class="row">
+    <div class="col-lg-12">
+
+        <b-alert variant="danger" :show="!!errorDelta">{{errorDelta}}</b-alert>
+    </div>
+
+    <div class="row mt-5">
       <b-button  type="submit" variant="secondary" @click="submit" class="m-auto">Submit</b-button>
       <b-button variant="secondary" @click="showPreview" class="m-auto">preview</b-button>
     </div>
@@ -83,7 +93,11 @@ export default{
         theme: 'snow'
       },
       delta: undefined,
-      mounting: false
+      mounting: false,
+      errorTitle: '',
+      errorDescription: '',
+      errorImageUrl: '',
+      errorDelta:''
     } 
   },
   watch:{
@@ -101,7 +115,14 @@ export default{
       const user = JSON.parse(localStorage.getItem('user'))
       // console.log('data', JSON.parse(localStorage.getItem('user')));
       const formData = {title: this.title, description: this.description, image_url: this.image_url, delta: delta, userId: user.id};
-      await this.$store.dispatch('setCreatePost', formData);
+      await this.$store.dispatch('setCreatePost', formData).then(success => {
+        console.log('success response', success);
+      }).catch(err => {
+        this.errorTitle = err.response.data.errors.title[0];
+        this.errorDescription = err.response.data.errors.description[0];
+        this.errorImageUrl = err.response.data.errors.image_url[0];
+        this.errorDelta = err.response.data.errors.delta[0];
+      });
     },
     showPreview(){
       this.$router.replace('/preview')
